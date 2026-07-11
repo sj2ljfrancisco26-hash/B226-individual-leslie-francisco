6bdcf8ff93473977cfede662a1c953604a4a9fa8
sj2ljfrancisco26-hash <sj2.ljfrancisco26@joysistvi.edu.ph>
2026-07-11 17:14:50 +0800

    day21 activity and discussion

--- src/day21/discussion/encapsulation/day20_sample.java
+++ src/day21/discussion/encapsulation/day20_sample.java
@@ -1,14 +1,69 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package day21.discussion.encapsulation;
 
-/**
- *
- * @author Iielsel
- */
 public class day20_sample {
+    public static void main(String[] args) {
+        System.out.println("==Encapsulation==");
+        Ballpen ballpen = new Ballpen();
+        ballpen.setBrand("Uno");
+        ballpen.setColor("Pink");
+        ballpen.setPrice(299);
+        ballpen.setPointSize(0.3);
+        
+        System.out.println("Brand: "+ ballpen.getBrand());
+        System.out.println("Color: "+ ballpen.getColor());
+        System.out.println("Price: "+ ballpen.getPrice());
+        System.out.println("Pointsize: "+ ballpen.getPointSize());
+        System.out.println();
+        
+        System.out.println("==Constructor Sample==");
+        Person person1 = new Person("Leslie", 27, "Pasig");
+        System.out.println("Name: " + person1.name + "\nAge: " + person1.age + "\nOrigin: " + person1.origin);
+        
+        Person person2 = new Person("SpongeBob", 24, "Bikini Bottom");
+        System.out.println("\nName: " + person2.name + "\nAge: " + person2.age + "\nOrigin: " + person2.origin);
+    }
+}
+class Ballpen{
     
+    String brand;
+    String color;
+    double price;
+    double pointSize;
+    
+    public void setBrand(String brand){
+        this.brand = brand;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+    public void setPointSize(double pointSize){
+        this.pointSize = pointSize;
+    }
+    
+    public String getBrand(){
+        return brand;
+    }
+    public String getColor(){
+        return color;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public double getPointSize(){
+        return pointSize;
+    }
+}
+class Person{
+    String name;
+    int age;
+    String origin;
+        public Person(String name, int age, String origin){
+            this.name = name;
+            this.age = age;
+            this.origin = origin;
+    }
 }
